255dd760ccd71d149ca2f81086dccb40405c77a1
Andreas Handel <andreas.handel@gmail.com>
2018-09-07 07:43:27 -0400

    modularizing ui, other mods

--- inst/createui/uiapp.R
+++ inst/createui/uiapp.R
@@ -1,54 +1,7 @@
-#the main function with all the functionality for the server
-#this function is wrapped inside the shiny server function below
-#to allow return to main menu when window is closed
-refresh <- function(input, output) {
-
-} #ends the 'refresh' shiny server function that runs the simulation and returns output
-
-
 server <- function(input, output, session)
 {
-    ###########################################
-    #server part that dynamically creates the UI
-    output$vars <- renderUI({
-        nvars = length(model$var)  #number of variables/compartments in model
-        allv = lapply(1:nvars, function(n) {
-            numericInput(model$var[[n]]$varname,
-                         model$var[[n]]$vartext,
-                         value = model$var[[n]]$varval)
-        })
-        do.call(mainPanel, allv)
-    })
 
-    output$pars <- renderUI({
-        npars = length(model$par)  #number of parameters in model
-        allp = lapply(1:npars, function(n) {
-            numericInput(
-                model$par[[n]]$parname,
-                model$par[[n]]$partext,
-                value = model$par[[n]]$parval,
-                step = model$par[[n]]$parval
-            )
-        })
-        do.call(mainPanel, allp)
-    })
-
-    output$time <- renderUI({
-        ntime = length(model$time)  #number of time variables in model
-        allt = lapply(1:ntime, function(n) {
-            numericInput(
-                model$time[[n]]$timename,
-                model$time[[n]]$timetext,
-                value = model$time[[n]]$timeval,
-                step = model$time[[n]]$timeval
-            )
-        })
-        do.call(mainPanel, allt)
-    })
-
-    output$title <- renderUI({
-        HTML(model$title)
-    }) #creates title
+    makeui(model, output) #make the UI for the model
 
     ###########################################
     #server part that listens for exit button click
@@ -63,65 +16,23 @@ server <- function(input, output, session)
     #on click, run simulation and update plots and text
     observeEvent(input$submitBtn,
     {
-         #save all results to a list for processing plots and text
-         listlength = 1
-         #here we do all simulations in the same figure
-         result = vector("list", listlength) #create empty list of right size for results
+        #save all results to a list for processing plots and text
+        listlength = 1
+        #here we do all simulations in the same figure
+        result = vector("list", listlength) #create empty list of right size for results
 
-         #process all variables, parameters and times from the model structure
-         #to create the input string for the main function call
-         nvars = length(model$var)
-         varstring = "vars = c("
-         for (n in 1:nvars)
-         {
-             varstring = paste0(varstring,
-                                model$var[[n]]$varname,
-                                " = ",
-                                isolate(input[[model$var[[n]]$varname]]),
-                                ', ')
-         }
-         varstring = substr(varstring, 1, nchar(varstring) - 2)
-         varstring = paste0(varstring, '), ') #close parentheses
+        #parses the model and creates the code to call/run the simulation
+        fctcall <- make_fctcall(input=input,model=model,modeltype='desolve')
 
-         npars = length(model$par)
-         parstring = "pars = c("
-         for (n in 1:npars)
-         {
-             parstring = paste0(parstring,
-                                model$par[[n]]$parname,
-                                " = ",
-                                isolate(input[[model$par[[n]]$parname]]),
-                                ', ')
-         }
-         parstring = substr(parstring, 1, nchar(parstring) - 2)
-         parstring = paste0(parstring, '), ') #close parentheses
+        #run simulation, show a 'running simulation' message
+        withProgress(message = 'Running Simulation', value = 0,
+        {
+             eval(parse(text = fctcall)) #execute function
+        })
 
-         ntime = length(model$time)
-         timestring = "time = c("
-         for (n in 1:ntime)
-         {
-             timestring = paste0(timestring,
-                                 model$time[[n]]$timename,
-                                 " = ",
-                                 isolate(input[[model$time[[n]]$timename]]),
-                                 ', ')
-         }
-         timestring = substr(timestring, 1, nchar(timestring) - 2)
-         timestring = paste0(timestring, ') ')
-
-         filename = paste0(gsub(" ", "_", model$title), "_desolve") #name of function to be called
-         fctargs = paste0(varstring, parstring, timestring) #arguments for function
-         fctcall = paste0('simresult <- ', filename, '(', fctargs, ')') # complete string for function call
-
-         #run simulation, show a 'running simulation' message
-         withProgress(message = 'Running Simulation', value = 0,
-                      {
-                          eval(parse(text = fctcall)) #execute function
-                      })
-
-         #data for plots and text
-         #needs to be in the right format to be passed to generate_plots and generate_text
-         #see documentation for those functions for details
+        #data for plots and text
+        #needs to be in the right format to be passed to generate_plots and generate_text
+        #see documentation for those functions for details
         result[[1]]$dat = simresult$ts
         result[[1]]$maketext = TRUE
 
