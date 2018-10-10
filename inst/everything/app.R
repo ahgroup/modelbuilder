db44f7ed22cabdacf0943c2c9ec2213cbb255d99
SpencerDH <sd77hall@uga.edu>
2018-10-10 10:20:24 -0400

    Updates

--- inst/everything/app.R
+++ inst/everything/app.R
@@ -51,7 +51,9 @@ server <- function(input, output, session) {
 
   observeEvent(input$process, {
       wd <- getwd()
-      print(input[[model$var[[1]]$varname]]) # Debugging line
+      print(d)
+      print(input[[d]])
+      print(input[[d$var[[1]]$varname]]) # Debugging line
       analyze_model(wd = wd, modeltype = input$modeltype,
                     rngseed = input$rngseed, nreps = input$nreps,
                     plotscale = input$plotscale, input = input)
