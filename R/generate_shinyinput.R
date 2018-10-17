1c0d342743e0c2138373a6064d256426d041a9cb
SpencerDH <sd77hall@uga.edu>
2018-10-17 10:27:21 -0400

    Updates

--- R/generate_shinyinput.R
+++ R/generate_shinyinput.R
@@ -11,7 +11,7 @@
 
 generate_shinyinput <- function(model, output)
 {
-    print(model) ### Debugging line
+    # print(model) ### Debugging line
     ###########################################
     #server part that dynamically creates the UI
     output$vars <- renderUI({
