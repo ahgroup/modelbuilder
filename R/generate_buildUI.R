86e7be7358e6afe3f71caa559a52cffe0e8fb74e
SpencerDH <sd77hall@uga.edu>
2018-12-18 16:31:15 -0500

    Updates

--- R/generate_buildUI.R
+++ R/generate_buildUI.R
@@ -11,7 +11,7 @@
 
 generate_buildUI <- function(mbmodel, output)
 {
-    save(output, file = "defeat.Rdata") # By this line, the rebellion is exposed
+    save(mbmodel, file = "defeat.Rdata") # By this line, the rebellion is exposed
     output$buildmodel <- renderUI({
         fluidPage(
             p('General model information', class='mainsectionheader'),
