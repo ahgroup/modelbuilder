b29d080a782ac34804c69b1b061e36463e5ea373
Andreas Handel <andreas.handel@gmail.com>
2020-09-30 09:17:40 -0400

    edits toward fixing stratification

--- auxiliary/vaccine_model_testing/model_analysis_script.R
+++ auxiliary/vaccine_model_testing/model_analysis_script.R
@@ -7,10 +7,15 @@ library("here") #for easy path access relative to project root directory
 #specify the simulation model code to use
 #the model/code has likely been generated with the generation script, but that doesn't need to be the case
 #any code for an mbmodel (or really any code function that has the right structure) should work
+#they are assumed to live in the 'stratified_models' folder
+modelname <- "Coronavirus_vaccine_model_v2_risk_age"
+#modelname <- "SIRSd_model_risk_age"
 
-
-modelname <- here("auxiliary/vaccine_model_testing/stratified_models","simulate_Coronavirus_vaccine_model_v2_stratified_stratified_ode.R")
-source(modelname)
+#generate file name for ODE model, also assumed to be in stratified_models folder
+#then source the file/function
+path = here("code/stratified_models")
+modelfilename <- paste0(path,"/simulate_",modelname,"_ode.R")
+source(modelfilename)
 
 #these are initial and parameter value spreadsheets
 #they contain the parameters for the model
@@ -20,13 +25,14 @@ source(modelname)
 #also note that the files are not read from the folder into which they are placed when
 #they are produced with the generation script. this is to prevent accidental overwrites
 #of already filled tables
-inivalfile = here('auxiliary/vaccine_model_testing/filled_tables','variable_table_Coronavirus_vaccine_model_v2_stratified_stratified.csv')
-parvalfile = here('auxiliary/vaccine_model_testing/filled_tables','parameter_table_Coronavirus_vaccine_model_v2_stratified_stratified.csv')
+path = here("code/filled_tables")
+inivalfile = paste0(path,'/variable_table_',modelname,'.csv')
+parvalfile = paste0(path,'/parameter_table_',modelname,'.csv')
 
 inivals_raw <- read.csv(inivalfile)
 parvals_raw <- read.csv(parvalfile)
 
-#those csv files contain columns that are not needed to run the model
+#those csv files may contain columns that are not needed to run the model
 #these lines of code get the initial values and parameter values into the right format
 #also time values (start/final/dt) are produced
 inivals = inivals_raw$Initial_Value
@@ -37,11 +43,8 @@ parvals = parvals_raw$Initial_Value
 names(parvals) = parvals_raw$Abbreviation
 parvals = as.list(parvals)
 
-mbmodel = readRDS("auxiliary/vaccine_model_testing/stratified_models/Coronavirus_vaccine_model_v2_stratified_stratified.Rds")
-timeinfo = mbmodel$time
-timevals = list(tstart = timeinfo[[1]]$timeval,
-                tfinal = timeinfo[[2]]$timeval,
-                dt = timeinfo[[3]]$timeval)
+#set times for simulation
+timevals = list(tstart = 0, tfinal = 100, dt = 0.1)
 
 #combine all the value lists into one large argument list
 #this is fed into the function that contains the ode model
@@ -49,23 +52,25 @@ args_list <- c(inivals, parvals, timevals)
 
 #loop over some parameter we want to explore
 #remaining parameter are fixed based on loaded values
-sigma_l_e_vec = seq(0,1,length=10)  #this is a placeholder
+par_vec = seq(0,1,length=10)  
 
 #will contain outcome of interest
-outcome = rep(0,length(sigma_l_e_vec))
+outcome = rep(0,length(par_vec))
 
-for (i in 1:length(sigma_l_e_vec))
+for (i in 1:length(par_vec))
 {
-
-  args_list$sigma_l_e <- sigma_l_e_vec[i]  #substitute the value for the parameter we want to change
-
+  
+  #args_list$w_l_e <- par_vec[i]  #substitute the value for the parameter we want to change
+  args_list$nu_Sl_Il_S_le_I_lc <- par_vec[i]  #substitute the value for the parameter we want to change
+  
   #run model
-  sim = do.call(simulate_Coronavirus_vaccine_model_v2_stratified_stratified_ode, args_list)
+  sim = do.call(paste0("simulate_",modelname,"_ode"), args_list)
   #pull out some part of interest from the simulation
   #the return from the function (i.e. what's stored in sim)
   #is always a list, with $ts the time-series matrix (what one gets if one just ran deSolve)
   outcome[i] = tail(sim$ts$S_l_e,1) #final number of susceptible
 }
 
-plot(sigma_e_l_vec, outcome,type='b')
+plot(par_vec, outcome,type='b')
+
 
--- auxiliary/vaccine_model_testing/model_generation_script.R
+++ auxiliary/vaccine_model_testing/model_generation_script.R
@@ -10,7 +10,9 @@ library("here") #for easy path access relative to project root directory
 
 #load the base model we want to work with
 #needs to be a modelbuilder mbmodel
-modelname <- here("auxiliary", "modelfiles","Coronavirus_vaccine_model_v2.Rds")
+#modelname <- here("code", "base_models","Coronavirus_vaccine_model_v2.Rds")
+#modelname <- here("auxiliary/vaccine_model_testing", "base_models","SIRSd_model.Rds")
+modelname <- here("auxiliary/vaccine_model_testing", "base_models","SIRSd2.Rds")
 mbmodel <- readRDS(modelname)
 
 #this runs the model through a checker to make sure it has a valid structure
@@ -23,13 +25,29 @@ if (!is.null(checkerror))
 }
 
 # If base model is valid, we can go on to specify the stratifications we want
-# Set up the stratification list
-# This is just an example...
 
-# create list mapping parameter stratifications to state variable
-par_stratify_list <- generate_stratifier_list(mbmodel)
+# First, we specify for each parameter the variables according to which it sould be stratified
+# calling the function below generates defaults
+# as default, every parameter is stratified based on the variables in the flow it appears
+# e.g. if we had dS/dt = n - m*S - b*S*I
+# the parameter n would not be stratified at all
+# the parameter m would be stratified by S
+# the parameter b would be stratified by S and I
+# the following function sends in a model and returns the variables by which each parameter should be stratified
+# the function returns a list, which each main list entry containing 2 elements,
+# the name of the parameter and the name of the variables by which it should be stratified
+# this list can be modified by the user before it is supplied to the function that generates the stratified model
+par_stratify_list <- modelbuilder::generate_stratifier_list(mbmodel)
 
-strata_list <- list(
+
+# Next, the user specifies the strata which should be generated
+# This needs to be done manually for each stratification
+# The structure is a list that contains the label given to the stratification,
+# the name for each stratum, and labels, which will be appended to all stratified model parameters and variables
+
+# This is an example of such a stratification with 2 risk groups
+
+stratum_list <- list(
   stratumname = "risk",
   names = c("high risk", "low risk"),
   labels = c("h", "l"),
@@ -38,24 +56,53 @@ strata_list <- list(
 
 # Now expand the model by strata the specified strata
 # The model that is returned is again a modelbuilder structure (a long list object)
+# The new name/title of the model is the old one appended with _stratumname
+# All stratified parameters and variables are set to the same value as the original unstratified ones
 mbmodel_new <- modelbuilder::generate_stratified_model(
-  mbmodel = mbmodel,
-  stratum_list = strata_list,
-  par_stratify_list = par_stratify_list
+                                  mbmodel = mbmodel,
+                                  stratum_list = stratum_list,
+                                  par_stratify_list = par_stratify_list
 )
 
-# now stratify again by age
+#code snippets to look at old and new model parameters
+#for development/testing only
+#unlist(lapply(mbmodel$par, "[[", "parname"))
+#unlist(lapply(mbmodel_new$par, "[[", "parname"))
+
+#check to make sure newly generated model is still a valid modelbuilder object
+#if not valid, a hopefully meaningful error message is returned
+# currently doesn't stop script, so rest will likely not work either
+checkerror <- modelbuilder::check_model(mbmodel_new)
+if (!is.null(checkerror))
+{
+  print(checkerror)
+}
+
+
+#######################################################
+#The stratification procedure is repeated for every new stratification we want to include
+# Since the model produced by the generate_stratified_model function has exactly the same mbmodel structure as the start model,
+# everything proceeds the same as going from the base model to the first stratified one
+# the order of stratification should not matter, the produced model should be the same
+# however, the naming of parameter and variables will change based on stratification order
+# labels for new strata are always appended at the end of any variable/parameter name
+
+# First we again generate a list of parameters and the variables according to which stratification should occur
+# Could be adjusted manually if wanted
+par_stratify_list <- modelbuilder::generate_stratifier_list(mbmodel_new)
+
+# now specify next level of stratification, this time done by age
 stratum_list <- list(
   stratumname = "age",
   names = c("children", "adults", "elderly"),
   labels = c("c", "a", "e"),
   comment = "This defines the age structure."
 )
-par_stratify_list <- generate_stratifier_list(mbmodel_new)
 
-final_model <- generate_stratified_model(mbmodel = mbmodel_new,
-                                         stratum_list = stratum_list,
-                                         par_stratify_list = par_stratify_list)
+final_model <- modelbuilder::generate_stratified_model(
+                                        mbmodel = mbmodel_new,
+                                        stratum_list = stratum_list,
+                                        par_stratify_list = par_stratify_list)
 
 #check to make sure newly generated model is still a valid modelbuilder object
 #if not valid, a hopefully meaningful error message is returned
@@ -66,23 +113,39 @@ if (!is.null(checkerror))
   print(checkerror)
 }
 
-#save newly generated model into rds file
-filename = paste0(final_model$title,'.Rds')
-saveRDS(final_model, file = here("auxiliary/vaccine_model_testing/stratified_models", filename))
+# More stratifications could be applied here, following the same pattern as above
+# 1. Specify for each parameter the variables that it should be stratified on
+# This can happen with the generate_stratifier_list function or manually (or a mix of those)
+# 2. Specify strata as a list
+# 3. Send strata list and stratifier list to generate_stratified_model function
 
-# run a function that generates CSV files for initial conditions and paramaeters
-# are being filled with the current values of mbmodel, can be adjusted
-savelocation = here('auxiliary/vaccine_model_testing/generated_tables/')
+
+# Once all stratifications are done, save the new model
+# Also generate/save tables of initial conditions and parameter values for manual filling
+
+#save newly generated model as Rds file
+#place it into the stratified models folder
+filename = paste0(final_model$title,'.Rds')
+saveRDS(final_model, file = here("code/stratified_models", filename))
+
+# run a function that generates CSV files for initial conditions and parameters
+# this function works for any modelbuilder object
+# Parameter and initial condition values are set to the values of mbmodel
+# these spreadsheets are meant to be moved by hand to the filled_tables folder and filled
+savelocation = here('code/generated_tables/')
 modelbuilder::generate_tables(mbmodel = final_model, location = savelocation)
 
 # run a function that generates a text file of all the flows in the model
+# this is not needed for any functionality
+# this can be useful for development/debugging
 # this can be useful for checking subscripting
-savelocation = here('auxiliary/vaccine_model_testing/stratified_models/')
-modelbuilder::export_flows(mbmodel = final_model, location = savelocation)
+#savelocation = here('code/stratified_models/')
+#modelbuilder::export_flows(mbmodel = final_model, location = savelocation)
 
 #create R code for the model
-#will be saved to the current directory
-savelocation = here('auxiliary/vaccine_model_testing/stratified_models/')
+#this takes the model and writes the R code for a function implemented as ODE or discrete time or stochastic model
+#the function will be saved to the current directory
+savelocation = here('code/stratified_models/')
 modelbuilder::generate_ode(mbmodel = final_model, location = savelocation)
 
 #at this stage the new model has been generated and saved as an Rds file
@@ -90,4 +153,5 @@ modelbuilder::generate_ode(mbmodel = final_model, location = savelocation)
 #code that implements the model as a set of ODEs has been generated
 
 #Next step is to use the model, as illustrated in the model_analysis_script
+
 
